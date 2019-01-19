5ea10d9bb35eda960d21e84933b1b32c49783b38
Prabhutva <42607655+Prabhutva@users.noreply.github.com>
2019-01-20 02:52:20 +0530

    Homepage UI

--- app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/HomeFragment.java
+++ app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/HomeFragment.java
@@ -6,12 +6,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
 import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Fresco.initialize(getContext());
         return inflater.inflate(R.layout.home_fragment,null);
     }
 }
