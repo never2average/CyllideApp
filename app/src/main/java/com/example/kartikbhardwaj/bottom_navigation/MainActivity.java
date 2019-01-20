9d57c70fe4765645aade9e46d87f0916d2175cbd
never2average <priyesh.sriv2017@gmail.com>
2019-01-20 17:23:20 +0300

    androidx stats page

--- app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/MainActivity.java
+++ app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/MainActivity.java
@@ -1,22 +1,16 @@
 package com.example.kartikbhardwaj.bottom_navigation;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
-import android.support.v7.widget.Toolbar;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
@@ -29,12 +23,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView =(BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView =findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
         loadfragment(new HomeFragment());
 
-        toolbar = (Toolbar)findViewById(R.id.tool_bar);
+        toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
         imageButton=findViewById(R.id.user_image);
@@ -90,7 +84,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 break;
 
             case R.id.stats:
-                getSupportActionBar().hide();
                 fragment = new StatsFragment();
                 break;
         }
--- app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/Profile_Activity.java
+++ app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/Profile_Activity.java
@@ -1,8 +1,7 @@
 package com.example.kartikbhardwaj.bottom_navigation;
 
+
 import android.content.Intent;
-import android.provider.ContactsContract;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -10,25 +9,29 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Profile_Activity extends AppCompatActivity {
-LinearLayout l1;
-Animation downtoup;
-ImageButton cross;
+
+    ImageButton cross;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_);
-  l1=findViewById(R.id.layout_profile);
-   downtoup=AnimationUtils.loadAnimation(this,R.anim.downtoup);
-      cross=findViewById(R.id.cross_btn);
-       cross.setOnClickListener(new View.OnClickListener() {
+        cross=findViewById(R.id.cross_btn);
+        cross.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
-        Intent exitIntent= new Intent(Profile_Activity.this,MainActivity.class);
-        startActivity(exitIntent);
-        finish();
+            Intent exitIntent= new Intent(Profile_Activity.this,MainActivity.class);
+            startActivity(exitIntent);
+            finish();
+            }
+        });
     }
 
-});
+    @Override
+    protected void onResume() {
+        super.onResume();
+        overridePendingTransition(R.anim.uptodown,R.anim.downtoup);
     }
 }
