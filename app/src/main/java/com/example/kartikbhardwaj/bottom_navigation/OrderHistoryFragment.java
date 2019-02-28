849e9a7bbd0ca6b6beafad74c9dcdec4cc49f66f
never2average <priyesh.sriv2017@gmail.com>
2019-02-28 15:46:33 +0300

    Commiting Changes

--- app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/OrderHistoryFragment.java
+++ app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/OrderHistoryFragment.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 
 
 
-public class OrderHistory extends Fragment {
+public class OrderHistoryFragment extends Fragment {
 
 
     @Override
--- app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/Portfolio/PortfolioActivity.java
+++ app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/Portfolio/PortfolioActivity.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.TextView;
@@ -24,19 +25,17 @@ import android.widget.Toolbar;
 
 import com.example.kartikbhardwaj.bottom_navigation.HomeFragment;
 import com.example.kartikbhardwaj.bottom_navigation.MainActivity;
+import com.example.kartikbhardwaj.bottom_navigation.OrderHistoryFragment;
 import com.example.kartikbhardwaj.bottom_navigation.PasswordChangeStatus;
 import com.example.kartikbhardwaj.bottom_navigation.R;
 import com.example.kartikbhardwaj.bottom_navigation.StatsFragment;
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.navigation.NavigationView;
 
 
 public class PortfolioActivity extends AppCompatActivity {
 
-    String stockName;
-    String newStockName;
-    String  buttonStatus;
-    String rvStatus;
-
+    MaterialCardView stockAnalysis, orderHistory, portfolioPositions;
 
 
     @Override
@@ -44,8 +43,6 @@ public class PortfolioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolio);
 
-        stockName = getIntent().getStringExtra("stock_name");
-        newStockName=getIntent().getStringExtra("newStockName");
         WebView webView = (WebView) findViewById(R.id.web_view_chart_portfolio);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/tt.html");
@@ -53,52 +50,40 @@ public class PortfolioActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
 
+        stockAnalysis = findViewById(R.id.stockchooser);
+        stockAnalysis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AvailableStocksFragment fragment1=new AvailableStocksFragment();
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container1,fragment1);
+                fragmentTransaction.commit();
+            }
+        });
 
+        orderHistory = findViewById(R.id.order_history);
+        orderHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OrderHistoryFragment fragment1 = new OrderHistoryFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container1, fragment1);
+                fragmentTransaction.commit();
+            }
+        });
 
-
-//        AvailableStocksFragment fragment=new AvailableStocksFragment();
-//        FragmentManager fragmentManager=getSupportFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        fragmentTransaction.add(R.id.container1,fragment);
-//        fragmentTransaction.commit();
-//        }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//
-//            case R.id.stockanalysis:
-//
-//                AvailableStocksFragment fragment1=new AvailableStocksFragment();
-//                FragmentManager fragmentManager=getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.container1,fragment1);
-//                fragmentTransaction.commit();
-//                return true;
-//
-//            case R.id.portfoliopositions:
-//
-//                PortfolioPositionsFragment fragment=new PortfolioPositionsFragment();
-//                FragmentManager fragmentManager1=getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction1 = fragmentManager1.beginTransaction();
-//                fragmentTransaction1.replace(R.id.container1,fragment);
-//                fragmentTransaction1.commit();
-//                item.setChecked(true);
-//                return true;
-//
-//            case R.id.orderspending:
-//
-//                PendingOrdersFragment fragment3=new PendingOrdersFragment();
-//                FragmentManager fragmentManager3=getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction3 = fragmentManager3.beginTransaction();
-//                fragmentTransaction3.replace(R.id.container1,fragment3);
-//                fragmentTransaction3.commit();
-//                item.setChecked(true);
-//                return true;
-//
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-
-
+        portfolioPositions = findViewById(R.id.portfolio_positions);
+        portfolioPositions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PortfolioPositionsFragment fragment1 = new PortfolioPositionsFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container1, fragment1);
+                fragmentTransaction.commit();
+            }
+        });
+    }
 }
