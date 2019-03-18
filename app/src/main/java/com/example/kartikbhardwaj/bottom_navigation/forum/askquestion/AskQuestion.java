51bf9782a54c10dac26f35566bcadc82f3318563
never2average <priyesh.sriv2017@gmail.com>
2019-03-18 18:15:43 +0530

    Ask A question UI completed

---
+++ app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/forum/askquestion/AskQuestion.java
@@ -0,0 +1,17 @@
+package com.example.kartikbhardwaj.bottom_navigation.forum.askquestion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kartikbhardwaj.bottom_navigation.R;
+
+public class AskQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_question);
+
+    }
+}
