ef0ef1dc87d5338a6f744ccd330436fd096e1849
MysteriousAcadia <anshumansingh64@gmail.com>
2019-03-29 04:06:57 +0530

    Integrate Quiz

--- app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/quiz/QuizActivity.java
+++ app/src/main/java/com/example/kartikbhardwaj/bottom_navigation/quiz/QuizActivity.java
@@ -1,6 +1,11 @@
 package com.example.kartikbhardwaj.bottom_navigation.quiz;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.kartikbhardwaj.bottom_navigation.R;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
@@ -14,6 +19,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,12 +39,14 @@ public class QuizActivity extends AppCompatActivity {
     int progressStatus = 0;
     CountDownTimer countDownTimer;
     CircularProgressBar circularProgressBar;
-    ProgressBar barTimer;
     String selectedOption = "noOption";
     TextView mainQuestion, optionA, optionB, optionC, optionD, textTimer;
     CardView option1CV,option2CV,option3CV,option4CV;
     RequestQueue quizAnswersRequestQueue;
+    JSONArray jsonQuestionArray;
     Map<String,String> answerRequestHeader = new ArrayMap<>();
+    ProgressBar option1PB, option2PB, option3PB, option4PB;
+    private int questionID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +61,11 @@ public class QuizActivity extends AppCompatActivity {
         option2CV = findViewById(R.id.question_activity_option_2_card_view);
         option3CV = findViewById(R.id.question_activity_option_3_card_view);
         option4CV = findViewById(R.id.question_activity_option_4_card_view);
+        option1PB = findViewById(R.id.option_1_progress_bar);
+        option2PB = findViewById(R.id.option_2_progress_bar);
+        option3PB = findViewById(R.id.option_3_progress_bar);
+        option4PB = findViewById(R.id.option_4_progress_bar);
+
 
         option1CV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,29 +103,14 @@ public class QuizActivity extends AppCompatActivity {
         String allQuestions = intent.getStringExtra("questions");
         Log.d("Questions",allQuestions);
         try {
-            JSONArray jsonQuestionArray = new JSONObject(allQuestions).getJSONArray("data");
-            JSONObject jsonObject = jsonQuestionArray.getJSONObject(0);
-            mainQuestion.setText(jsonObject.getString("theQuestion"));
-            JSONArray answerArray = jsonObject.getJSONArray("answerOptions");
-            optionA.setText(answerArray.getJSONObject(0).getString("value"));
-            optionB.setText(answerArray.getJSONObject(1).getString("value"));
-            optionC.setText(answerArray.getJSONObject(2).getString("value"));
-            optionD.setText(answerArray.getJSONObject(3).getString("value"));
-            circularProgressBar.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
-            circularProgressBar.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent));
-            circularProgressBar.setProgressBarWidth(10);
-            circularProgressBar.setBackgroundProgressBarWidth(10);
-            circularProgressBar.setProgressWithAnimation(100, 20000);
-            startTimer(10);
-
-
+            JSONObject responseObject = new JSONObject(allQuestions);
+            jsonQuestionArray = responseObject.getJSONArray("data");
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        changeQuestion(questionID);
 
 
-        int animationDuration = 10000; // 2500ms = 2,5s
-        ; // Default duration = 1500ms
 
 
     }
@@ -133,9 +131,22 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 if(textTimer.getText().equals("00:00")){
+                    showAnswer(questionID);
                     textTimer.setText("STOP");
-                    quizAnswersRequestQueue = Volley.newRequestQueue(getBaseContext());
-                    answerRequestHeader.put("","")
+                    if(checkAnswer(questionID)){
+                        if(questionID == 9){
+                            finishQuiz(questionID);
+                        }
+                        else{
+                            questionID += 1;
+                            changeQuestion(questionID);
+                        }
+
+                    }
+                    else{
+                        finishQuiz(questionID);
+                    }
+
 
 
 
@@ -146,6 +157,59 @@ public class QuizActivity extends AppCompatActivity {
                 }
             }
         }.start();
+
+    }
+
+    private boolean checkAnswer(int questionID){
+//        if(selectedOption)
+        String url = "http://api.cyllide.com/api/client/quiz/submit";
+        try {
+            JSONObject quizObject = jsonQuestionArray.getJSONObject(questionID);
+            String id = quizObject.getString();//WRITE STRING NAME
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String id;
+                //TODO write volley here
+        return true;
+    }
+
+    private void changeQuestion(int questionID){
+        JSONObject jsonObject = null;
+        try {
+            circularProgressBar.setProgress(0);
+            jsonObject = jsonQuestionArray.getJSONObject(questionID);
+            mainQuestion.setText(jsonObject.getString("theQuestion"));
+            JSONArray answerArray = jsonObject.getJSONArray("answerOptions");
+            optionA.setText(answerArray.getJSONObject(0).getString("value"));
+            optionB.setText(answerArray.getJSONObject(1).getString("value"));
+            optionC.setText(answerArray.getJSONObject(2).getString("value"));
+            optionD.setText(answerArray.getJSONObject(3).getString("value"));
+            circularProgressBar.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
+            circularProgressBar.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent));
+            circularProgressBar.setProgressBarWidth(10);
+            circularProgressBar.setBackgroundProgressBarWidth(10);
+            circularProgressBar.setProgressWithAnimation(100, 20000);
+            startTimer(10);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void finishQuiz(int questionID){
+        if(questionID == 9){
+            Toast.makeText(this,"You  lol",Toast.LENGTH_LONG);
+        }
+        else{
+            Toast.makeText(this,"You Win lol",Toast.LENGTH_LONG);
+        }
+    }
+
+    private void showAnswer(int questionID){
 
     }
 
