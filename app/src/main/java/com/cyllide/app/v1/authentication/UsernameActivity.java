67be1f1db8a3d87088b45af2736d459c6036b926
MysteriousAcadia <anshumansingh64@gmail.com>
2019-05-12 01:14:27 +0530

    Create username page

---
+++ app/src/main/java/com/cyllide/app/v1/authentication/UsernameActivity.java
@@ -0,0 +1,16 @@
+package com.cyllide.app.v1.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cyllide.app.v1.R;
+
+public class UsernameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_username);
+    }
+}
--- app/src/main/java/com/cyllide/app/v1/quiz/QuizRulesActivity.java
+++ app/src/main/java/com/cyllide/app/v1/quiz/QuizRulesActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.collection.ArrayMap;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
 import android.app.Dialog;
 import android.app.NotificationChannel;
@@ -53,8 +54,10 @@ public class QuizRulesActivity extends AppCompatActivity {
     ImageView backButton;
     Calendar startTime = Calendar.getInstance();
     private Map<String,String> questionHeaders = new ArrayMap<String, String>();
+    RequestQueue currentTimeRequestQueue;
     RemoteViews contentView;
     CountDownTimer quizCountDownTimer;
+    long currentTime;
 
     @Override
     protected void onPause(){
@@ -98,43 +101,62 @@ public class QuizRulesActivity extends AppCompatActivity {
 
         Intent serviceIntent = new Intent(this, GetLatestQuizIDService.class);
         startService(serviceIntent);
+        currentTimeRequestQueue = Volley.newRequestQueue(this);
+        String url = getResources().getString(R.string.currentTimeApiURL);
+        StringRequest currentTimeStringRequest = new StringRequest(Request.Method.GET,url , new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    currentTime = (new JSONObject(response).getLong("unixtime"))* 1000;
+                    Log.d("QuizRulesActivity", Long.toString(currentTime));
+                    Log.d("QuizRulesActivity",Long.toString(new JSONObject(response).getLong("unixtime")));
+                    createTimer();
+                } catch (JSONException e) {
+                    Log.d("QuizRulesActivity", e.toString());
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VOLLEY", error.toString());
+            }
+        })
+        {
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse nr) {
+                int n = nr.statusCode;
+                Log.d("Res Code",""+n);
+                return super.parseNetworkResponse(nr);
+            }
+
+        };
+
+        currentTimeRequestQueue.add(currentTimeStringRequest);
 
 
-
-//        contentView = new RemoteViews(getPackageName(), R.layout.push_notification_layout);
-//        contentView.setImageViewResource(R.id.image, R.mipmap.ic_launcher);
-//        contentView.setTextViewText(R.id.title, "Polish your Finance-Whiz");
-//        contentView.setTextViewText(R.id.text, "Quiz starts in 5 minutes");
-
-//        String CHANNEL_ID = "LOL";
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-//                .setSmallIcon(R.drawable.icon_logo)
-//                .setContentTitle("Polish your finance-whiz")
-//                .setContentText("Quiz starts in 5 minutes")
-//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-//
-//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-// notificationId is a unique int for each notification that you must define
-//        notificationManager.notify(1000, builder.build());
 
 
         startQuizButton=findViewById(R.id.startQuizButton);
 
-//        startQuizButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                fetchQuestions(quizID);
-//
-//            }
-//        });
 
         quizPrize = findViewById(R.id.quiz_rules_qrize_text_view);
         backButton = findViewById(R.id.activity_quiz_rules_back_button);
         SharedPreferences sharedPreferences = getSharedPreferences("LATESTQUIZ", 0);
         quizStartTime = Long.parseLong(sharedPreferences.getString("time","0"));
-        if (quizStartTime != 0 && quizStartTime-System.currentTimeMillis() < 0){
-            Toast.makeText(QuizRulesActivity.this,"No Quizes Available", Toast.LENGTH_LONG).show();
+
+
+
+    }
+
+
+    private void createTimer()
+    {
+        if (quizStartTime != 0 && quizStartTime-currentTime < 0){
+            Toast.makeText(QuizRulesActivity.this,"No Quizzes Available", Toast.LENGTH_LONG).show();
             startActivity(new Intent(QuizRulesActivity.this,MainActivity.class));
             finish();
         }
@@ -165,7 +187,7 @@ public class QuizRulesActivity extends AppCompatActivity {
                     try {
                         quizID = new JSONObject(response).getJSONObject("data").getJSONObject("_id").getString("$oid");
                         quizStartTime = new JSONObject(response).getJSONObject("data").getJSONObject("quizStartTime").getLong("$date");
-                         SharedPreferences.Editor edit = getSharedPreferences("LATESTQUIZ",0).edit();
+                        SharedPreferences.Editor edit = getSharedPreferences("LATESTQUIZ",0).edit();
                         edit.putString("time",Long.toString(quizStartTime));
                         edit.putString("id",quizID);
                         edit.apply();
@@ -173,46 +195,37 @@ public class QuizRulesActivity extends AppCompatActivity {
 
                         Log.d("Response", quizID);
                         Log.d("Response", Long.toString(quizStartTime));
-                        Log.d("Timer",Long.toString(quizStartTime-System.currentTimeMillis()));
+                        Log.d("Timer",Long.toString(quizStartTime-currentTime));
                         quizCountDownTimer =
-                        new CountDownTimer(quizStartTime-System.currentTimeMillis(),1000){
-                            @Override
-                            public void onTick(long millisUntilFinished) {
-                                String hour=String.valueOf(millisUntilFinished/(1000*3600));
-                                String days = String.valueOf(Integer.parseInt(hour)/24);
-                                String hours = String.valueOf(Integer.parseInt(hour)%24);
-                                String minute=String.valueOf((millisUntilFinished/(1000*60))%60);
-                                String second=String.valueOf(((millisUntilFinished/1000)%60)%60);
-                                SimpleDateFormat df =new SimpleDateFormat("MM:SS");
-                                if(Integer.parseInt(hour)>24){
-//                                    df = new SimpleDateFormat("DD:HH");
-                                }
-                                else if(Integer.parseInt(hour)<24 && Integer.parseInt(hour)>=1){
-//                                    df = new SimpleDateFormat("HH hours : MM minutes");
-                                }
+                                new CountDownTimer(quizStartTime-currentTime,1000){
+                                    @Override
+                                    public void onTick(long millisUntilFinished) {
+                                        String hour=String.valueOf(millisUntilFinished/(1000*3600));
+                                        String days = String.valueOf(Integer.parseInt(hour)/24);
+                                        String hours = String.valueOf(Integer.parseInt(hour)%24);
+                                        String minute=String.valueOf((millisUntilFinished/(1000*60))%60);
+                                        String second=String.valueOf(((millisUntilFinished/1000)%60)%60);
+                                        if(Integer.parseInt(days)>0){
+                                            startQuizButton.setText("Quiz Starts in "+days+" days "+hours+" hours ");
+                                        }
+                                        else if(Integer.parseInt(days)<=0 && Integer.parseInt(hours)>0){
+                                            startQuizButton.setText("Quiz Starts in "+hours+" hours "+minute+" minutes ");
+                                        }
+                                        else if(Integer.parseInt(hours)<=0 && Integer.parseInt(minute)>0){
+                                            startQuizButton.setText("Quiz Starts in "+minute+" minutes "+second+" seconds ");
+                                        }
+                                        else{
+                                            startQuizButton.setText("Quiz Starts in "+second+" seconds ");
+                                        }
+                                        startQuizButton.setClickable(true);
 
-                                String time=df.format(millisUntilFinished);
-                                if(Integer.parseInt(days)>0){
-                                startQuizButton.setText("Quiz Starts in "+days+" days "+hours+" hours ");
-                                }
-                                else if(Integer.parseInt(days)<=0 && Integer.parseInt(hours)>0){
-                                    startQuizButton.setText("Quiz Starts in "+hours+" hours "+minute+" minutes ");
-                                }
-                                else if(Integer.parseInt(hours)<=0 && Integer.parseInt(minute)>0){
-                                    startQuizButton.setText("Quiz Starts in "+minute+" minutes "+second+" seconds ");
-                                }
-                                else{
-                                    startQuizButton.setText("Quiz Starts in "+second+" seconds ");
-                                }
-                                startQuizButton.setClickable(true);
+                                    }
 
-                            }
-
-                            @Override
-                            public void onFinish() {
-                                  fetchQuestions(quizID);
-                            }
-                        }.start();
+                                    @Override
+                                    public void onFinish() {
+                                        fetchQuestions(quizID);
+                                    }
+                                }.start();
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -244,13 +257,6 @@ public class QuizRulesActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-//        startQuizButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                fetchQuestions(quizID);
-//            }
-//        });
 
     }
 
